798ec807fa599ae49fe760134a06bd17e8669c04
Microanswer <microanswer@outlook.com>
2018-12-04 18:21:33 +0800

    sss

---
+++ src/main/java/cn/microanswer/Test38.java
@@ -0,0 +1,17 @@
+package cn.microanswer;
+
+
+public class Test38 {
+
+    /**
+     *
+     * @param args 主参数
+     * @author Microanswer
+     * @date 2018年12月04日 16:58:39
+     */
+    public static void main(String[] args) throws Exception {
+
+    	int a = 100/'i';
+        
+    }
+}
