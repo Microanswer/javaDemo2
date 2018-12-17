eaedbef2f52ce62cbe856867e4aa4f59750fe775
Microanswer <microanswer@outlook.com>
2018-12-17 18:12:46 +0800

    代码更新

--- src/main/java/cn/microanswer/Test41.java
+++ src/main/java/cn/microanswer/Test41.java
@@ -3,20 +3,28 @@ package cn.microanswer;
 
 public class Test41 {
 
+    static {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("我日");
+    }
+    static String res = wori();
+
+
     /**
      * @param args 主参数
      * @author Microanswer
      * @date 2018年12月14日 10:45:35
      */
     public static void main(String[] args) throws Exception {
-        String str = "";
-
-        str = uperFirst(str);
-
-        System.out.println(str);
+        System.out.println("我在主方法里：" + res);
     }
 
-    public static String uperFirst(String str) {
-        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    public static String wori () {
+        System.out.println("我在方法里日");
+        return "wori";
     }
 }
