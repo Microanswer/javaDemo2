86d7a1988552685e5097b2fee84ac2300e3f6d68
Microanswer <microanswer@outlook.com>
2019-01-02 19:11:34 +0800

    代码更新

---
+++ src/main/java/cn/microanswer/Test47.java
@@ -0,0 +1,16 @@
+package cn.microanswer;
+
+
+public class Test47 {
+
+    /**
+     *
+     * @param args 主参数
+     * @author Microanswer
+     * @date 2019年01月02日 17:41:07
+     */
+    public static void main(String[] args) throws Exception {
+        String value = "2019";
+        
+    }
+}
