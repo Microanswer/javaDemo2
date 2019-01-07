44d0fc4904e94ef3aab7c4a73d5fafcbc94367ca
Microanswer <microanswer@outlook.com>
2019-01-07 18:14:16 +0800

    代码更新

--- src/main/java/cn/microanswer/Test49.java
+++ src/main/java/cn/microanswer/Test49.java
@@ -2,13 +2,12 @@ package cn.microanswer;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
 
 public class Test49 {
 
     /**
+     *
+     * 泛型的理解。
      *
      * @param args 主参数
      * @author Microanswer
@@ -16,14 +15,35 @@ public class Test49 {
      */
     public static void main(String[] args) throws Exception {
 
-        List<Integer> nus = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 300000; i++) {
-            nus.add(random.nextInt(Integer.MAX_VALUE));
+    }
+
+
+    /**
+     * 要理解泛型，不能直接从代码入手。 你要先从白话文上理解啥是泛型。
+     *
+     * 举例： 有一个背包，这个背包可以装很多东西，具体这个背包装的是什么东西。不清楚。 但是我们可以在使用背包的时候
+     *       告诉背包我们要装什么东西。 那么由于要告诉背包我们装什么东西。所以在程序上来说，需要有一个东西来保存我们告
+     *       诉背包这是什么东西的变量，这个变量，就是俩尖括号里面的内容。
+     *
+     * 下面定义 背包 类， 这个‘背包’可以装任何东西， 背包就设定这个东西是 T 类型的(也可以是别的字母，不一定得是T这个字母)。
+     *
+     * @param <T>
+     */
+    static class BackPack<T/*使用T来表示背包要存放的物品类型，尖括号就是这样的语法，记住就好了*/> {
+
+        /**
+         * 既然我们有了 T 来标致物品的类型， 所以就直接用 T 来声明一个数组， 保存的东西就好方便存在这个数组里面。
+         */
+        T contents[];
+
+        /**
+         * 咱们的背包是有限制容量的， 通过构造函数指定这个背包最多可以放多少个物品。
+         * @param maxCount
+         */
+        BackPack(int maxCount) {
+            // 根据这个容量进行初始化数组。
+            contents = (T[]) new Object[maxCount];
         }
 
-        long s = System.currentTimeMillis();
-        Collections.sort(nus);
-        System.out.println("耗时：" + (System.currentTimeMillis() - s) + "毫秒");
     }
 }
