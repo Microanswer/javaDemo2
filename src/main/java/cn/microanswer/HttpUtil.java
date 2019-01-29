5ba01334d5b59115f6bf65ca21a227eac9f01e78
Microanswer <microanswer@outlook.com>
2019-01-29 10:35:44 +0800

    代码更新+优化httputil

--- src/main/java/cn/microanswer/HttpUtil.java
+++ src/main/java/cn/microanswer/HttpUtil.java
@@ -5,7 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import okhttp3.*;
 import okio.BufferedSink;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URLEncoder;
 import java.util.Map;
 import java.util.Set;
@@ -30,12 +30,21 @@ public class HttpUtil {
     /**
      * 使用 get方法 请求某地址。
      *
-     * @param url     [Y] 要请求的地址。
-     * @param headers [N] 请求头。
-     * @return 请求结果。
-     * @throws Exception 错误。
+     * @param url     [Y] 要请求的地址
+     * @param params  [N] 请求参数
+     * @param headers [N] 请求头
+     * @return
      */
-    public static String get(String url, Map<String, String> headers) throws Exception {
+    public static String get(String url, Map<String, String> params, Map<String, String> headers) throws Exception {
+        String queryString = map2wwwUrlFormEncode(params);
+        if (queryString.length() > 0) {
+            if (url.contains("?")) {
+                url = url + "&" + queryString;
+            } else {
+                url = url + "?" + queryString;
+            }
+        }
+
         Request.Builder builder = new Request.Builder();
         builder.url(url);
         if (headers != null && headers.size() > 0) {
@@ -52,6 +61,18 @@ public class HttpUtil {
             throw new Exception("request error:" + url + " [" + response.code() + "] " + response.message());
         }
         return response.body().string();
+    }
+
+    /**
+     * 使用 get方法 请求某地址。
+     *
+     * @param url    [Y] 要请求的地址。
+     * @param params [N] 请参数。
+     * @return 请求结果。
+     * @throws Exception 错误。
+     */
+    public static String get(String url, Map<String, String> params) throws Exception {
+        return get(url, params, null);
     }
 
     /**
@@ -231,6 +252,130 @@ public class HttpUtil {
         return postApplicationXml(url, xmlStr, null);
     }
 
+    /**
+     * 下载文件.如果指定的文件已存在，将会覆盖。
+     *
+     * @param url       [Y] 下载路径
+     * @param params    [N] 参数
+     * @param headers   [N] 请求头
+     * @param fileOrDir [N] 下载目录或直接对应文件。
+     * @return
+     */
+    public static File download(String url, Map<String, String> params, Map<String, String> headers, File fileOrDir) throws Exception {
+        String queryString = map2wwwUrlFormEncode(params);
+        if (queryString.length() > 0) {
+            if (url.contains("?")) {
+                url = url + "&" + queryString;
+            } else {
+                url = url + "?" + queryString;
+            }
+        }
+
+        Request.Builder builder = new Request.Builder();
+        builder.url(url);
+        if (headers != null && headers.size() > 0) {
+            Set<Map.Entry<String, String>> entries = headers.entrySet();
+            for (Map.Entry<String, String> entry : entries) {
+                builder.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
+        builder.get();
+        Request request = builder.build(); // 建立请求数据。
+        Call call = getHttpClient().newCall(request); // 使用请求数据建立请求。
+        Response response = call.execute(); // 发起请求。
+        if (response.code() != 200) {
+            throw new Exception("download error:" + url + " [" + response.code() + "] " + response.message());
+        }
+
+        if (fileOrDir == null || fileOrDir.isDirectory()) {
+
+            // 处理要保存的文件路径。
+            String fileName = "";
+
+            // 从响应头获取一个文件名：
+            String contentdisposition = response.header("Content-Disposition");
+            if (contentdisposition != null) {
+                int filenameIndex = contentdisposition.indexOf("filename");
+                if (filenameIndex > -1) {
+                    fileName = contentdisposition.substring(filenameIndex + 10, contentdisposition.length() - 1);
+                }
+            }
+
+            // 没有获取到文件名则从url中解析文件名。
+            if (fileName.length() < 1) {
+                // 从 url 检索一个文件名：
+                String[] split = url.split("\\?")[0].split("/");
+                fileName = split[split.length - 1];
+            }
+
+            // 依然没有文件名，只有使用时间戳来命名了。
+            if (fileName.length() < 1) {
+                fileName = System.currentTimeMillis() + ".httputildownload";
+            }
+
+            if (fileOrDir == null) {
+                // 如果没有指定，那么将文件保存到临时目录
+                File tempDir = new File(System.getProperty("java.io.tmpdir"));
+                fileOrDir = new File(tempDir, fileName);
+            } else {
+                // 指定的是目录
+                fileOrDir = new File(fileOrDir, fileName);
+            }
+        }
+
+        // 开始下载
+        InputStream inputStream = response.body().byteStream(); // 下载输入流获取到。
+
+        FileOutputStream outputStream = new FileOutputStream(fileOrDir);
+        BufferedOutputStream bfo = new BufferedOutputStream(outputStream);
+
+        byte[] data = new byte[1024];
+        int dataSize;
+
+        while ((dataSize = inputStream.read(data)) != -1) {
+            bfo.write(data, 0, dataSize);
+        }
+
+        bfo.flush();
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+
+        return fileOrDir;
+    }
+
+    /**
+     * 将 url 的文件下载到 fileOrDir 指定的路径。如果指定的文件已存在，将会覆盖。
+     *
+     * @param url       [Y] url下载路径
+     * @param fileOrDir [N] 目录或文件保存路径，如果不传，将保存在零时目录
+     * @return
+     */
+    public static File download(String url, File fileOrDir) throws Exception {
+        return download(url, null, null, fileOrDir);
+    }
+
+    /**
+     * 将 url 的文件下载到 fileOrDir 指定的路径。如果指定的文件已存在，将会覆盖。
+     *
+     * @param url           [Y] url下载路径
+     * @param fileOrDirPath [N] 目录或文件保存路径，如果不传，将保存在零时目录
+     * @return
+     */
+    public static File download(String url, String fileOrDirPath) throws Exception {
+        return download(url, new File(fileOrDirPath));
+    }
+
+    /**
+     * 下载 url 对应的文件。 文件将被保存到零时文件夹。如果指定的文件已存在，将会覆盖。
+     *
+     * @param url [Y] 下载路径
+     * @return
+     * @throws Exception
+     */
+    public static File download(String url) throws Exception {
+        return download(url, (File)null);
+    }
 
     /**
      * 将 map 集合转换为 www-url-form-encoded 格式的字符串。
@@ -254,15 +399,15 @@ public class HttpUtil {
         return stringBuilder.toString();
     }
 
-
     /**
      * 将 map 集合转换为 www-url-form-encoded 格式的字符串。。 默认使用 UTF-8 编码
      *
-     * @param params  [N] 要转换的集合map
+     * @param params [N] 要转换的集合map
      * @return 结果
      * @throws Exception
      */
     public static String map2wwwUrlFormEncode(Map<String, ?> params) throws Exception {
         return map2wwwUrlFormEncode(params, CHARSET);
     }
+
 }
--- src/main/java/cn/microanswer/Test51.java
+++ src/main/java/cn/microanswer/Test51.java
@@ -1,50 +1,6 @@
 package cn.microanswer;
 
-import static cn.microanswer.U.print;
-
-class Mug {
-    Mug(int marker) {
-        print("Mug(" + marker + ")");
-    }
-
-    void f(int marker) {
-        print("f(" + marker + ")");
-    }
-}
-
-class Mugs {
-    Mug mug1;
-    Mug mug2;
-
-    // {   在编译成 class 文件后，这里的构造代码块的代码就会被移动到 每一个构造函数里头。
-    //     就像下面的构造函数一样
-    //     mug1 = new Mug(1);
-    //     mug2 = new Mug(2);
-    //     print("mug1 & mug2 initialized");
-    // }
-
-    Mugs() {
-
-        // 先前构造代码块的代码在被编译成 class 后实际上代码就成了这样的了
-        mug1 = new Mug(1);
-        mug2 = new Mug(2);
-        print("mug1 & mug2 initialized");
-
-        print("Mugs()");
-    }
-
-    Mugs(int i) {
-
-        // 先前构造代码块的代码在被编译成 class 后实际上代码就成了这样的了
-
-        mug1 = new Mug(1);
-        mug2 = new Mug(2);
-        print("mug1 & mug2 initialized");
-
-        print("Mugs(int)");
-    }
-
-}
+import java.io.File;
 
 public class Test51 {
 
@@ -54,15 +10,8 @@ public class Test51 {
      * @date 2019年01月21日 14:58:56
      */
     public static void main(String[] args) throws Exception {
-        String s = HttpUtil.get("https://baidu.com", null);
-        System.out.println(s);
+        File s = HttpUtil.download("http://amazeui.org/javascript/popover");
+        System.out.println(s.getCanonicalPath());
     }
 
-}
-
-
-class U {
-    public static void print(String va) {
-        System.out.println(va);
-    }
 }
