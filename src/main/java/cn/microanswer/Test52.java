9a289f38928c970da7a2d3d0e9f2dfa8d336b170
Microanswer <microanswer@outlook.com>
2019-02-20 18:14:28 +0800

    代码更新

---
+++ src/main/java/cn/microanswer/Test52.java
@@ -0,0 +1,13 @@
+package cn.microanswer;
+
+
+public class Test52 {
+
+    /**
+     * @param args 主参数
+     * @author Microanswer
+     * @date 2019年02月18日 16:56:24
+     */
+    public static void main(String[] args) throws Exception {
+    }
+}
